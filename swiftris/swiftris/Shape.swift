99697d21374c797545eb6334a7b50df085adfc9c
Cooper Geng <cooper.geng@fuhu.com>
2015-05-08 11:35:50 -0700

    update code

---
+++ swiftris/swiftris/Shape.swift
@@ -0,0 +1,44 @@
+//
+//  Shape.swift
+//  swiftris
+//
+//  Created by Yong on 5/7/15.
+//  Copyright (c) 2015 Yong. All rights reserved.
+//
+
+import SpriteKit
+
+let NumOrientations: UInt32 = 4
+
+enum Orientation: Int, Printable {
+    case Zero = 0, Ninety, OneEighty, TwoSeventy
+    
+    var description: String {
+        switch self {
+        case .Zero:
+            return "0"
+        case .Ninety:
+            return "90"
+        case .OneEighty:
+            return "180"
+        case .TwoSeventy:
+            return "270"
+        }
+    }
+    
+    static func random() -> Orientation {
+        return Orientation(rawValue: Int(arc4random_uniform(NumOrientations)))!
+    }
+    
+    static func rotate(orientation: Orientation, clockwise: Bool) -> Orientation {
+        var rotated = orientation.rawValue + (clockwise ? 1 : -1)
+        if rotated > Orientation.TwoSeventy.rawValue {
+            rotated = Orientation.Zero.rawValue
+        } else if rotated < 0 {
+            rotated = Orientation.TwoSeventy.rawValue
+        }
+        return Orientation(rawValue: rotated)!
+    }
+}
+
+let NumShapeTypes: UInt32 = 7
