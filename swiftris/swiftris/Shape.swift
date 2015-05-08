7b2cf3a443156f60c0d455c92743b0bdcfa292fe
Cooper Geng <cooper.geng@fuhu.com>
2015-05-08 14:43:40 -0700

    Add shape file

--- swiftris/swiftris/Shape.swift
+++ swiftris/swiftris/Shape.swift
@@ -42,3 +42,72 @@ enum Orientation: Int, Printable {
 }
 
 let NumShapeTypes: UInt32 = 7
+
+let FirstBlockIdx: Int = 0
+let SecondBlockIdx: Int = 1
+let ThirdBlockIdx: Int = 2
+let FourthBlockIdx: Int = 3
+
+class Shape: Hashable, Printable {
+    let color: BlockColor
+    
+    var blocks = Array<Block>()
+    
+    var orientation: Orientation
+    
+    var column, row: Int
+    
+    var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [:]
+    }
+    
+    var bottomBlocksForOrientation: [Orientation: Array<Block>] {
+        return [:]
+    }
+    
+    var bottomBlocks: Array<Block> {
+        if let bottomBlocks = bottomBlocksForOrientation[orientation] {
+            return bottomBlocks
+        }
+        return []
+    }
+    
+    var hashValue: Int {
+        return reduce(blocks, 0) { $0.hashValue ^ $1.hashValue }
+    }
+    
+    
+    var description:String {
+        return "\(color) block facing \(orientation): \(blocks[FirstBlockIdx]), \(blocks[SecondBlockIdx]), \(blocks[ThirdBlockIdx]), \(blocks[FourthBlockIdx])"
+    }
+    
+    init(column: Int, row: Int, color: BlockColor, orientation: Orientation) {
+        self.color = color
+        self.column = column
+        self.row = row
+        self.orientation = orientation
+        initializeBlocks()
+    }
+    
+    convenience init(column: Int, row: Int) {
+        self.init(column:column, row:row, color:BlockColor.random(), orientation:Orientation.random())
+    }
+    
+    final func initializeBlocks() {
+        if let blockRowColumnTranslations = blockRowColumnPositions[orientation] {
+            for i in 0..<blockRowColumnTranslations.count {
+                let blockRow = row + blockRowColumnTranslations[i].rowDiff
+                let blockColumn = column + blockRowColumnTranslations[i].columnDiff
+                let newBlock = Block(column: blockColumn, row: blockRow, color: color)
+                blocks.append(newBlock)
+            }
+        }
+    }
+}
+
+func == (lhs: Shape, rhs: Shape) -> Bool {
+    return lhs.row == rhs.row && lhs.column == rhs.column
+}
+
+
+
